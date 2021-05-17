025061f7cdac5b8be78c312ebbe9abbdca1e45c7
Inatan <inatan_hertzog@hotmail.com>
2021-05-17 20:29:07 -0300

    Finish report challenge

--- lib/Bookings/report.ex
+++ lib/Bookings/report.ex
@@ -2,17 +2,24 @@ defmodule Flightex.Bookings.Report do
   alias Flightex.Bookings.Agent, as: BookingAgent
   alias Flightex.Bookings.Booking
 
-  def create(filename \\ "report.csv") do
-    booking_list = build_booking_list()
+  # filename \\ "report.csv"
+  def generate_report(from_date, to_date) do
+    booking_list = build_booking_list(from_date, to_date)
+
+    filename = "#{Date.to_string(from_date)}-#{Date.to_string(to_date)}.csv"
 
     File.write(filename, booking_list)
 
     {:ok, "Report generated successfully"}
   end
 
-  defp build_booking_list do
+  defp build_booking_list(from_date, to_date) do
     BookingAgent.get_all()
     |> Map.values()
+    |> Enum.filter(fn x ->
+      NaiveDateTime.compare(x.data_completa, from_date) == :gt and
+        NaiveDateTime.compare(x.data_completa, to_date) == :lt
+    end)
     |> Enum.map(fn booking -> booking_string(booking) end)
   end
 
--- test/bookings/report_test.exs
+++ test/bookings/report_test.exs
@@ -9,16 +9,25 @@ defmodule Flightex.Bookings.ReportTest do
   describe "create/1" do
     test "create the report file" do
       BookingAgent.start_link(%{})
-
-      booking1 = build(:booking)
+      idUser = UUID.uuid4()
+      booking1 = build(:booking, id_usuario: idUser, id: "1")
       BookingAgent.save(booking1)
 
-      expected_response =
-        {:ok, "#{booking1.id},2000-01-01 23:00:07,Porto Alegre,Curicica,#{booking1.id_usuario}\n"}
+      booking2 =
+        build(:booking, id_usuario: idUser, data_completa: ~N[2010-01-01 22:00:07], id: "2")
 
-      Report.create("Report_test.csv")
+      BookingAgent.save(booking2)
 
-      response = File.read("Report_test.csv")
+      booking3 =
+        build(:booking, id_usuario: idUser, data_completa: ~N[2012-01-01 22:00:07], id: "3")
+
+      BookingAgent.save(booking3)
+
+      expected_response = {:ok, "#{idUser},Porto Alegre,Curicica,2010-01-01 22:00:07\n"}
+
+      Report.generate_report(~N[2010-01-01 00:00:07], ~N[2011-01-01 23:00:07])
+
+      response = File.read("2010-01-01-2011-01-01.csv")
 
       assert response == expected_response
     end
